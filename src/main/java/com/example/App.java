e141b6d8e92c8f278c839534fc519159b68787b3
Nandeesh-Gopal <nandeeshgopal26@gmail.com>
2026-04-20 12:16:40 +0530

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println(" MAVEN BUILD AUTOMATION PROJECT ");
+        System.out.println("==================================");
+        System.out.println("Project Name : Maven Demo");
+        System.out.println("Build Status : SUCCESS");
+        System.out.println("Jar Created Successfully");
+        System.out.println("Application Running Perfectly");
+
+    }
+}
