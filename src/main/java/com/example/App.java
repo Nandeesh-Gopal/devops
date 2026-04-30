ca45fea7f2d378ab08e20b0cb6e66d151c50016e
Nandeesh-Gopal <nandeeshgopal26@gmail.com>
2026-04-30 10:47:03 +0530

    this is sriram'

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("==================================");
-        System.out.println("devops project" );
+        System.out.println("devops project new file" );
         System.out.println("==================================");
         System.out.println("Project Name : Maven Demo");
         System.out.println("Build Status : SUCCESS");
