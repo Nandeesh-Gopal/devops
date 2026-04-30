5028e71e596b370882ae7f6172ff835fb372c99d
Nandeesh-Gopal <nandeeshgopal26@gmail.com>
2026-04-30 11:04:13 +0530

    nans commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -14,8 +14,15 @@ public class App {
         // Serve HTML
         server.createContext("/", exchange -> {
             InputStream is = App.class.getClassLoader().getResourceAsStream("index.html");
-            byte[] response = is.readAllBytes();
+ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+int nRead;
+byte[] data = new byte[1024];
 
+while ((nRead = is.read(data, 0, data.length)) != -1) {
+    buffer.write(data, 0, nRead);
+}
+
+byte[] response = buffer.toByteArray();
             exchange.sendResponseHeaders(200, response.length);
             OutputStream os = exchange.getResponseBody();
             os.write(response);
